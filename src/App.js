4462803ba16cb68f3710bb49d72082cad4580a19
tuansyho1996 <hosytuan101296@gmail.com>
2023-07-25 23:52:50 +0700

    make add todo use use useState  with array spread syntax array

--- src/App.js
+++ src/App.js
@@ -7,19 +7,24 @@ const App = () => {
     let number = 2023;
     let obj = { name: 'Tuan', title: 'Learn reactjshook basic' };
     let link = "https://reactjs.org";
-    let [name, setName] = useState('Tuan');
-    const handleClick = () => {
-        console.log('click me', name)
+    let [address, setAddress] = useState('');
+    let [todo, setTodo] = useState([
+        { id: 1, title: 'coding' }
+    ]);
+    const handleClick = async () => {
+        let newTodo = { id: todo.length + 1, title: address };
+        setTodo([...todo, newTodo]);
+        setAddress('');
     }
     const handleChange = (event) => {
-        setName(event.target.value);
+        setAddress(event.target.value);
     }
     return (
         <div className="App">
             <Navigation />
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>hello {name}</p>
+                <p>hello {obj.name}</p>
                 <div>
                     {obj.title} with {obj.name} in {number}
                 </div>
@@ -31,7 +36,16 @@ const App = () => {
                 >
                     Learn React
                 </a>
-                <input value={name} onChange={(event) => handleChange(event)} />
+                <div>
+                    {todo && todo.length &&
+                        todo.map(item => {
+                            return (
+                                <li className='element-todo' key={item.id}>{item.title}</li>
+                            )
+                        })
+                    }
+                </div>
+                <input value={address} onChange={(event) => handleChange(event)} />
                 <button onClick={() => handleClick()}>Click me</button>
             </header>
         </div>
