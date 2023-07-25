e90facfa4e77349f5b05f610bbc479f34a5892c8
tuansyho1996 <hosytuan101296@gmail.com>
2023-07-25 12:08:37 +0700

    render data of variable with react hook

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+    let name = 'tuan';
+    let number = 2023;
+    let obj = { name: 'Tuan', title: 'Learn reactjshook basic' };
+    let link = "https://reactjs.org"
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Learn reactjs hook basic
-                </p>
+                <p>hello {name}</p>
+                <div>
+                    {obj.title} with {obj.name} in {number}
+                </div>
                 <a
                     className="App-link"
-                    href="https://reactjs.org"
+                    href={link}
                     target="_blank"
                     rel="noopener noreferrer"
                 >
