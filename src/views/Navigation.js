5983e143d68c9ef62a861f71553cabbe666d9fbb
tuansyho1996 <hosytuan101296@gmail.com>
2023-07-25 20:47:21 +0700

    install npm sass use scc Navigation

--- src/views/Navigation.js
+++ src/views/Navigation.js
@@ -1,9 +1,9 @@
-import './Navigation.css'
+import './Navigation.scss'
 
 const Navigation = () => {
     return (
-        <div class="topnav">
-            <a class="active" href="#home">Home</a>
+        <div className="topnav">
+            <a className="active" href="#home">Home</a>
             <a href="#news">News</a>
             <a href="#contact">Contact</a>
             <a href="#about">About</a>
